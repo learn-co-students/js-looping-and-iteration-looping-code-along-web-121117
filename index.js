0643c754146727487ca82f660c327408fa89f5e1
johneckert <johnteckert@gmail.com>
2018-01-29 13:35:52 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function printBadges(arrayOfNames) {
+  for (let i = 0; i < arrayOfNames.length; i++) {
+    console.log(`Welcome ${arrayOfNames[i]}! You are employee #${i + 1}.`);
+  }
+  return arrayOfNames;
+};
+
+function tailsNeverFails() {
+  let count = 0;
+  while (Math.random() >= 0.5) {
+    count++;
+  };
+  return `You got ${count} tails in a row!`;
+};
