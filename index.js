7cc4d08d282c262a47fa1b617d099f517a58326d
github username <github email address>
2018-01-29 12:56:23 -0500

    Done.

--- index.js
+++ index.js
@@ -6,13 +6,24 @@ function printBadges(array){
   return array
 }
 
-function tailsNeverFails(){
-  coin_sides = ["Heads","Tails"]
-  let coin = coin_sides[Math.floor(Math.random() * coin_sides.length)]
-  let counter = 0
-  while(coin === "Tails"){
-    counter++
-    coin = coin_sides[Math.floor(Math.random() * coin_sides.length)]
+// function tailsNeverFails(){
+  // coin_sides = ["Heads","Tails"]
+  // let coin = coin_sides[Math.floor(Math.random() * coin_sides.length)]
+  // let counter = 0
+  // while(coin === "Tails"){
+  //   counter++
+  //   coin = coin_sides[Math.floor(Math.random() * coin_sides.length)]
+  // }
+  // return `You got ${counter} tails in a row!`
+// }
+function maybeTrue() {
+  return Math.random() >= 0.5;
+}
+
+function tailsNeverFails() {
+  let count = 0
+  while (maybeTrue()) {
+    count += 1
   }
-  return `You got ${counter} tails in a row!`
+  return `You got ${count} tails in a row!`
 }
