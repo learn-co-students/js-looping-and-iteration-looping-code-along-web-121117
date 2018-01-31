48ff39ada15ecb22c02d1dc371229a161fa7e55f
github username <github email address>
2018-01-31 11:49:31 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solutions in this file
+
+
+function tailsNeverFails(){
+  let i = 0
+  let x = 0
+  while (x < 0.5) {
+    x = Math.random();
+    i++
+  }
+  console.log(`You got ${i} tails in a row!`);
+}
+// tailsNeverFails()
